cabbb955f88061e319c8c563b7bcb825e73ea706
Erdal Güzel <erdal.guzel@ceng.deu.edu.tr>
2022-10-22 19:49:31 +0300

    Initial commit

---
+++ src/main/java/dev/springsecurity/demo/config/WebAppSecurityConfig.java
@@ -0,0 +1,26 @@
+package dev.springsecurity.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+import dev.springsecurity.demo.enumeration.UserRole;
+
+@Configuration
+@EnableWebSecurity
+public class WebAppSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+
+		auth.inMemoryAuthentication()
+				.withUser(userBuilder.username("john").password("test123").roles(UserRole.EMPLOYEE.getKey()))
+				.withUser(userBuilder.username("mary").password("test123").roles(UserRole.ADMIN.getKey()))
+				.withUser(userBuilder.username("karen").password("test123").roles(UserRole.MANAGER.getKey()));
+	}
+
+}
---
+++ src/main/java/dev/springsecurity/demo/enumeration/UserRole.java
@@ -0,0 +1,17 @@
+package dev.springsecurity.demo.enumeration;
+
+public enum UserRole {
+	EMPLOYEE("EMPLOYEE"),
+	MANAGER("MANAGER"),
+	ADMIN("ADMIN");
+	
+	private String key;
+	
+	private UserRole(String key) {
+		this.key=key;
+	}
+
+	public String getKey() {
+		return key;
+	}
+}
